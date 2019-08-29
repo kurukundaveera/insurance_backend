ad814db31d12dc5bfa5d10873ddc4348d7eba543
nithin2889 <nithin2889@gmail.com>
2019-08-29 02:50:33 -0800

    hari commited buy

--- src/main/java/com/hcl/einsurance/util/EinsuranceConstants.java
+++ src/main/java/com/hcl/einsurance/util/EinsuranceConstants.java
@@ -13,7 +13,8 @@ public class EinsuranceConstants {
 	public static final String POLICY_NOT_FOUND = "Policy not exist";
 	public static final String INVALID_MOBILENUMBER = "Enter 10 digits only!";
 	public static final String INVALID_NAME = "Enter alphabets only!";
-	public static final String DATE_FORMAT = "MM/dd/yyyy";
+	//public static final String DATE_FORMAT = "MM/dd/yyyy";
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
 	public static final String ERROR_AGE = "You are not satisfying the entry age limit of the specific policy.";
 	public static final String ERROR_POLICY_EXCEPTION = "You already purchased.";
 }
