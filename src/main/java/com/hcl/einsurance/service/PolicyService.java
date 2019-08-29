263f5b9cb72df03037ccd8af37b45339187122ec
squard4ing@gmail.com <venky.svreddy@gmail.com>
2019-08-29 13:46:15 +0530

    list of policies

--- src/main/java/com/hcl/einsurance/service/PolicyService.java
+++ src/main/java/com/hcl/einsurance/service/PolicyService.java
@@ -1,5 +1,11 @@
 package com.hcl.einsurance.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.hcl.einsurance.dto.PolicyResponseDto;
+@Service
 public interface PolicyService {
+	public List<PolicyResponseDto> getPolicyDetails(String all);
 
 }
--- src/main/java/com/hcl/einsurance/service/PolicyServiceImpl.java
+++ src/main/java/com/hcl/einsurance/service/PolicyServiceImpl.java
@@ -1,8 +1,34 @@
 package com.hcl.einsurance.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hcl.einsurance.dto.PolicyResponseDto;
+import com.hcl.einsurance.entity.Policies;
+import com.hcl.einsurance.repository.PolicyRepository;
 
 @Service
 public class PolicyServiceImpl implements PolicyService{
-
-}
+@Autowired PolicyRepository policyRepository;
+private static final Logger logger = LoggerFactory.getLogger(PolicyServiceImpl.class);
+ @Override
+	public List<PolicyResponseDto> getPolicyDetails(String all) {
+	 logger.info("in getPolicy method");
+		List<PolicyResponseDto> responseList = new ArrayList<>();
+		List<Policies> policyList;
+		if(all.equalsIgnoreCase("all")) {
+			policyList = policyRepository.findAll();
+			policyList.stream().forEach(c -> {
+				PolicyResponseDto response = new PolicyResponseDto();
+				BeanUtils.copyProperties(c, response);
+				responseList.add(response);
+			});
+		}
+		return responseList;
+ }}
